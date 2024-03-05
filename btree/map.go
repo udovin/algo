327f14b6766d97ebd137374b27dd03d7581b3935
Ivan Udovin <wilcot@ya.ru>
2024-03-05 13:51:38 +0300

    Optimize btree for large objects

--- btree/map.go
+++ btree/map.go
@@ -29,13 +29,9 @@ const (
 	minLen    = maxLen / 2
 )
 
-type mapItem[K, V any] struct {
-	value V
-	key   K
-}
-
 type mapNode[K, V any] struct {
-	items    [maxLen]mapItem[K, V]
+	keys     [maxLen]K
+	values   [maxLen]V
 	children *[maxLen + 1]*mapNode[K, V]
 	len      int
 }
@@ -55,7 +51,7 @@ func (m *mapImpl[K, V]) Get(key K) (V, bool) {
 	for {
 		i, ok := m.search(n, key)
 		if ok {
-			return n.items[i].value, true
+			return n.values[i], true
 		}
 		if n.children == nil {
 			return empty, false
@@ -65,8 +61,7 @@ func (m *mapImpl[K, V]) Get(key K) (V, bool) {
 }
 
 func (m *mapImpl[K, V]) Set(key K, value V) {
-	item := mapItem[K, V]{key: key, value: value}
-	m.setRootNode(item)
+	m.setRootNode(key, value)
 }
 
 func (m *mapImpl[K, V]) Delete(key K) {
@@ -94,86 +89,98 @@ func (m *mapImpl[K, V]) search(n *mapNode[K, V], key K) (int, bool) {
 	low, high := 0, n.len
 	for low < high {
 		mid := (low + high) / 2
-		if m.less(key, n.items[mid].key) {
+		if m.less(key, n.keys[mid]) {
 			high = mid
 		} else {
 			low = mid + 1
 		}
 	}
-	if low > 0 && !m.less(n.items[low-1].key, key) {
+	if low > 0 && !m.less(n.keys[low-1], key) {
 		return low - 1, true
 	}
 	return low, false
 }
 
-func (m *mapImpl[K, V]) setRootNode(item mapItem[K, V]) {
+func (m *mapImpl[K, V]) setRootNode(key K, value V) {
 	if m.root == nil {
 		m.root = &mapNode[K, V]{len: 1}
-		m.root.items[0] = item
+		m.root.keys[0] = key
+		m.root.values[0] = value
 		m.len = 1
 		return
 	}
-	split := m.setNode(&m.root, item)
+	split := m.setNode(&m.root, key, value)
 	if split {
 		left := m.root
-		mid, right := m.splitNode(left)
+		k, v, right := m.splitNode(left)
 		m.root = &mapNode[K, V]{len: 1}
-		m.root.items[0] = mid
+		m.root.keys[0] = k
+		m.root.values[0] = v
 		m.root.children = &[maxLen + 1]*mapNode[K, V]{left, right}
-		m.setRootNode(item)
+		m.setRootNode(key, value)
 	}
 }
 
-func (m *mapImpl[K, V]) setNode(p **mapNode[K, V], item mapItem[K, V]) bool {
+func (m *mapImpl[K, V]) setNode(p **mapNode[K, V], key K, value V) bool {
 	n := *p
-	i, ok := m.search(*p, item.key)
+	i, ok := m.search(*p, key)
 	if ok {
-		n.items[i] = item
+		n.keys[i] = key
+		n.values[i] = value
 		return false
 	}
 	if n.children == nil {
 		if n.len == maxLen {
 			return true
 		}
-		copy(n.items[i+1:], n.items[i:n.len])
-		n.items[i] = item
+		copy(n.keys[i+1:], n.keys[i:n.len])
+		copy(n.values[i+1:], n.values[i:n.len])
+		n.keys[i] = key
+		n.values[i] = value
 		n.len++
 		m.len++
 		return false
 	}
-	split := m.setNode(&n.children[i], item)
+	split := m.setNode(&n.children[i], key, value)
 	if split {
 		if n.len == maxLen {
 			return true
 		}
-		mid, right := m.splitNode(n.children[i])
-		copy(n.items[i+1:], n.items[i:n.len])
-		n.items[i] = mid
+		k, v, right := m.splitNode(n.children[i])
+		copy(n.keys[i+1:], n.keys[i:n.len])
+		copy(n.values[i+1:], n.values[i:n.len])
+		n.keys[i] = k
+		n.values[i] = v
 		copy(n.children[i+1:], n.children[i:n.len+1])
 		n.children[i+1] = right
 		n.len++
-		return m.setNode(p, item)
+		return m.setNode(p, key, value)
 	}
 	return false
 }
 
-func (m *mapImpl[K, V]) splitNode(n *mapNode[K, V]) (mapItem[K, V], *mapNode[K, V]) {
+func (m *mapImpl[K, V]) splitNode(n *mapNode[K, V]) (K, V, *mapNode[K, V]) {
 	i := maxLen / 2
-	mid := n.items[i]
+	key := n.keys[i]
+	value := n.values[i]
 	right := &mapNode[K, V]{len: n.len - i - 1}
-	copy(right.items[:], n.items[i+1:])
+	copy(right.keys[:], n.keys[i+1:])
+	copy(right.values[:], n.values[i+1:])
 	if n.children != nil {
 		right.children = &[maxLen + 1]*mapNode[K, V]{}
 		copy(right.children[:], n.children[i+1:])
 	}
+	var emptyKey K
+	var emptyValue V
 	for j := i; j < maxLen; j++ {
-		n.items[j] = mapItem[K, V]{}
+		n.keys[j] = emptyKey
+		n.values[j] = emptyValue
 		if n.children != nil {
 			n.children[j+1] = nil
 		}
 	}
 	n.len = i
-	return mid, right
+	return key, value, right
 }
 
 func (m *mapImpl[K, V]) deleteNode(p **mapNode[K, V], key K) bool {
@@ -181,9 +188,13 @@ func (m *mapImpl[K, V]) deleteNode(p **mapNode[K, V], key K) bool {
 	i, ok := m.search(n, key)
 	if n.children == nil {
 		if ok {
-			copy(n.items[i:], n.items[i+1:n.len])
+			copy(n.keys[i:], n.keys[i+1:n.len])
+			copy(n.values[i:], n.values[i+1:n.len])
 			n.len--
-			n.items[n.len] = mapItem[K, V]{}
+			var emptyKey K
+			var emptyValue V
+			n.keys[n.len] = emptyKey
+			n.values[n.len] = emptyValue
 			m.len--
 			return true
 		}
@@ -191,8 +202,7 @@ func (m *mapImpl[K, V]) deleteNode(p **mapNode[K, V], key K) bool {
 	}
 	deleted := false
 	if ok {
-		item := m.deleteMaxItem(n.children[i])
-		n.items[i] = item
+		n.keys[i], n.values[i] = m.deleteMaxItem(n.children[i])
 		m.len--
 		deleted = true
 	} else {
@@ -207,13 +217,17 @@ func (m *mapImpl[K, V]) deleteNode(p **mapNode[K, V], key K) bool {
 	return true
 }
 
-func (m *mapImpl[K, V]) deleteMaxItem(n *mapNode[K, V]) mapItem[K, V] {
+func (m *mapImpl[K, V]) deleteMaxItem(n *mapNode[K, V]) (K, V) {
 	for {
 		if n.children == nil {
 			n.len--
-			item := n.items[n.len]
-			n.items[n.len] = mapItem[K, V]{}
-			return item
+			key := n.keys[n.len]
+			value := n.values[n.len]
+			var emptyKey K
+			var emptyValue V
+			n.keys[n.len] = emptyKey
+			n.values[n.len] = emptyValue
+			return key, value
 		}
 		n = n.children[n.len]
 	}
@@ -227,51 +241,71 @@ func (m *mapImpl[K, V]) rebalanceNode(n *mapNode[K, V], i int) {
 	right := n.children[i+1]
 	if left.len+right.len < maxLen {
 		node := &mapNode[K, V]{len: left.len + right.len + 1}
-		copy(node.items[:], left.items[:left.len])
-		node.items[left.len] = n.items[i]
-		copy(node.items[left.len+1:], right.items[:right.len])
+		copy(node.keys[:], left.keys[:left.len])
+		copy(node.values[:], left.values[:left.len])
+		node.keys[left.len] = n.keys[i]
+		node.values[left.len] = n.values[i]
+		copy(node.keys[left.len+1:], right.keys[:right.len])
+		copy(node.values[left.len+1:], right.values[:right.len])
 		if left.children != nil {
 			node.children = &[maxLen + 1]*mapNode[K, V]{}
 			copy(node.children[:], left.children[:left.len+1])
 			copy(node.children[left.len+1:], right.children[:right.len+1])
 		}
-		copy(n.items[i:], n.items[i+1:n.len])
+		copy(n.keys[i:], n.keys[i+1:n.len])
+		copy(n.values[i:], n.values[i+1:n.len])
 		copy(n.children[i+1:], n.children[i+2:n.len+1])
 		n.children[i] = node
 		n.children[n.len] = nil
 		n.len--
-		n.items[n.len] = mapItem[K, V]{}
+		var emptyKey K
+		var emptyValue V
+		n.keys[n.len] = emptyKey
+		n.values[n.len] = emptyValue
 	} else if left.len > right.len {
-		copy(right.items[1:], right.items[:right.len])
-		right.items[0] = n.items[i]
+		copy(right.keys[1:], right.keys[:right.len])
+		copy(right.values[1:], right.values[:right.len])
+		right.keys[0] = n.keys[i]
+		right.values[0] = n.values[i]
 		right.len++
-		n.items[i] = left.items[left.len-1]
+		n.keys[i] = left.keys[left.len-1]
+		n.values[i] = left.values[left.len-1]
 		if left.children != nil {
 			copy(right.children[1:], right.children[:right.len])
 			right.children[0] = left.children[left.len]
 			left.children[left.len] = nil
 		}
 		left.len--
-		left.items[left.len] = mapItem[K, V]{}
+		var emptyKey K
+		var emptyValue V
+		left.keys[left.len] = emptyKey
+		left.values[left.len] = emptyValue
 	} else {
-		left.items[left.len] = n.items[i]
+		left.keys[left.len] = n.keys[i]
+		left.values[left.len] = n.values[i]
 		left.len++
-		n.items[i] = right.items[0]
-		copy(right.items[:], right.items[1:right.len])
+		n.keys[i] = right.keys[0]
+		n.values[i] = right.values[0]
+		copy(right.keys[:], right.keys[1:right.len])
+		copy(right.values[:], right.values[1:right.len])
 		if left.children != nil {
 			left.children[left.len] = right.children[0]
 			copy(right.children[:], right.children[1:right.len+1])
 			right.children[right.len] = nil
 		}
 		right.len--
-		right.items[right.len] = mapItem[K, V]{}
+		var emptyKey K
+		var emptyValue V
+		right.keys[right.len] = emptyKey
+		right.values[right.len] = emptyValue
 	}
 }
 
 type mapIter[K, V any] struct {
 	m      *mapImpl[K, V]
 	stack  []mapIterPos[K, V]
-	item   mapItem[K, V]
+	key    K
+	value  V
 	seeked bool
 }
 
@@ -294,7 +328,8 @@ func (m *mapIter[K, V]) First() bool {
 		}
 		n = n.children[0]
 	}
-	m.item = n.items[0]
+	m.key = n.keys[0]
+	m.value = n.values[0]
 	return true
 }
 
@@ -313,7 +348,8 @@ func (m *mapIter[K, V]) Last() bool {
 		}
 		n = n.children[n.len]
 	}
-	m.item = n.items[n.len-1]
+	m.key = n.keys[n.len-1]
+	m.value = n.values[n.len-1]
 	return true
 }
 
@@ -348,7 +384,8 @@ func (m *mapIter[K, V]) Next() bool {
 		}
 		s = &m.stack[len(m.stack)-1]
 	}
-	m.item = s.n.items[s.i]
+	m.key = s.n.keys[s.i]
+	m.value = s.n.values[s.i]
 	return true
 }
 
@@ -385,7 +422,8 @@ func (m *mapIter[K, V]) Prev() bool {
 		}
 		s = &m.stack[len(m.stack)-1]
 	}
-	m.item = s.n.items[s.i]
+	m.key = s.n.keys[s.i]
+	m.value = s.n.values[s.i]
 	return true
 }
 
@@ -400,7 +438,8 @@ func (m *mapIter[K, V]) Seek(key K) bool {
 		i, ok := m.m.search(n, key)
 		m.stack = append(m.stack, mapIterPos[K, V]{n, i})
 		if ok {
-			m.item = n.items[i]
+			m.key = n.keys[i]
+			m.value = n.values[i]
 			return true
 		}
 		if n.children == nil {
@@ -412,9 +451,9 @@ func (m *mapIter[K, V]) Seek(key K) bool {
 }
 
 func (m *mapIter[K, V]) Key() K {
-	return m.item.key
+	return m.key
 }
 
 func (m *mapIter[K, V]) Value() V {
-	return m.item.value
+	return m.value
 }
